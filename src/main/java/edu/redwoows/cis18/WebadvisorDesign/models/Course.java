113b2661dd5f6534b2e14221f7d613af3c7859d9
SivertLog <SivertLog@gmail.com>
2025-11-02 09:01:42 -0800

    trying to add stuff for course table

---
+++ src/main/java/edu/redwoows/cis18/WebadvisorDesign/models/Course.java
@@ -0,0 +1,65 @@
+package edu.redwoows.cis18.WebadvisorDesign.models;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
+    private Integer courseId;
+
+    @Column(name = "course_name")
+    private String courseName;
+
+    @Column(name = "course_instructor")
+    private String courseInstructor;
+
+    @Column(name = "course_time")
+    private String courseTime;
+
+    @Column(name = "course_day")
+    private String courseDay;
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseInstructor() {
+        return courseInstructor;
+    }
+
+    public void setCourseInstructor(String courseInstructor) {
+        this.courseInstructor = courseInstructor;
+    }
+
+    public String getCourseTime() {
+        return courseTime;
+    }
+
+    public void setCourseTime(String courseTime) {
+        this.courseTime = courseTime;
+    }
+
+    public String getCourseDay() {
+        return courseDay;
+    }
+
+    public void setCourseDay(String courseDay) {
+        this.courseDay = courseDay;
+    }
+}
